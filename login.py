dc1d490f3fccbd34f5cc5a074be2161970892576
LeDirecteur <erxx707@gmail.com>
2025-06-07 00:27:47 +0200

    Tuto login

---
+++ login.py
@@ -0,0 +1,58 @@
+import flet as ft 
+from flet import TextField, Checkbox, ElevatedButton, Text, Row, Column
+from flet import ControlEvent
+
+def main(page: ft.Page) -> None:
+
+    page.title = 'SignUp'
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.theme_mode = ft.ThemeMode.LIGHT
+    page.window_with = 400
+    page.window_height = 400
+    page.windows_resizable = False
+    
+    text_username: TextField = TextField(label='username', text_align=ft.TextAlign.LEFT, width=200)
+    text_password: TextField = TextField(label='password', text_align=ft.TextAlign.LEFT, width=200, password=True)
+    checkbox_signup : Checkbox = Checkbox(label='Jaccpete connard', value=False)
+    button_submit : ElevatedButton = ElevatedButton(text='Sign up', width=200,disabled=True)
+
+    def validate(e: ControlEvent) -> None:
+        if all([text_username.value , text_password.value, checkbox_signup.value]):
+            button_submit.disabled = False
+        else:
+            button_submit.disabled = True
+        page.update()
+
+    def submit(e: ControlEvent) -> None:
+        print('Username', text_username.value)
+        print('Username', text_username.value)
+
+        page.clean()
+        page.add(
+            Row(
+                controls=[Text(value=f'Welcome : {text_username.value}',size=20)],
+                alignment=ft.MainAxisAlignment.CENTER
+            )
+        )
+    checkbox_signup.on_change = validate
+    text_username.on_change = validate
+    text_password.on_change = validate
+    button_submit.on_click = submit
+
+    page.add(
+        Row(
+            controls=[
+                Column(
+                    [text_username,
+                     text_password,
+                     checkbox_signup,
+                     button_submit]
+                )
+            ],
+            alignment=ft.MainAxisAlignment.CENTER
+        )
+    )
+
+if __name__ == '__main__':
+    ft.app(target=main)
+
